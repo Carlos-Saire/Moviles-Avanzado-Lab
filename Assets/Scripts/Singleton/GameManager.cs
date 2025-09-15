d47863d699340f15c95396d10fe65427c2c551a7
Carlos Saire <carlos.saire@tecsup.edu.pe>
2025-09-15 15:56:48 -0500

    a

--- Assets/Scripts/Singleton/GameManager.cs
+++ Assets/Scripts/Singleton/GameManager.cs
@@ -1,11 +1,25 @@
 using UnityEngine;
 using Unity.Netcode;
-
+[RequireComponent(typeof(NetworkObject))]
 public class GameManager : NetworkBehaviour
 {
+    [Header("Prefab")]
+    [SerializeField] private Transform playerPregab;
+    
+    [SerializeField]
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
         Debug.Log(NetworkManager.Singleton.LocalClientId);
+        GeneratePlayerRpc();
+    }
+
+    [Rpc(SendTo.Server)]
+    private void GeneratePlayerRpc()
+    {
+        Transform player = Instantiate(playerPregab);
+        player.GetComponent<NetworkObject>().Spawn();
     }
 }
+
+
